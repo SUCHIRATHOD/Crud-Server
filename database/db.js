a8d84d140365b703f722beabf3d2318702da4359
SUCHIRATHOD <sulochanarathod58@gmail.com>
2024-10-28 11:23:24 +0530

    Update db.js

--- database/db.js
+++ database/db.js
@@ -2,11 +2,11 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb://localhost:27017/crud");
+        await mongoose.connect("mongodb+srv://sulochana:Sulo@2001@cluster0.4j7rt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
         console.log("MongoDB connected Successfully");
         
     }catch (error){
-        console.error(`MongoDB connection error: ${PORT}`);
+        console.error("MongoDB connection error: error");
     }
 }
 
