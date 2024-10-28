41ba22a17dd0d44df0caf57393659dfdd26fe717
SUCHIRATHOD <sulochanarathod58@gmail.com>
2024-10-28 11:27:39 +0530

    Update db.js

--- database/db.js
+++ database/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb+srv://sulochana:Sulo@2001@cluster0.4j7rt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+        await mongoose.connect("mongodb+srv://sulochana:Sulo@2001@cluster0.4j7rt.mongodb.net/crud?retryWrites=true&w=majority&appName=Cluster0");
         console.log("MongoDB connected Successfully");
         
     }catch (error){
